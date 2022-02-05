0ce47cabb74bb2999f48719756ef45fae89cb351
IvanMenas <ivanmenasalvarenga@gmail.com>
2022-02-05 12:05:55 -0600

    - files skeleton

---
+++ Ent_Semana3/Ent_Semana3/Controllers/TipoCambioController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Ent_Semana3.Controllers
+{
+    public class TipoCambioController : ApiController
+    {
+    }
+}
---
+++ Ent_Semana3/Ent_Semana3/Entities/TipoCambioObj.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ent_Semana3.Entities
+{
+    public class TipoCambioObj
+    {
+    }
+}

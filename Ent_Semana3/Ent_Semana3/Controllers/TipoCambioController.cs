324f0efe9a9692ad1e2575426d0907873543cfc0
IvanMenas <ivanmenasalvarenga@gmail.com>
2022-02-05 13:53:31 -0600

    - load WS data

--- Ent_Semana3/Ent_Semana3/Controllers/TipoCambioController.cs
+++ Ent_Semana3/Ent_Semana3/Controllers/TipoCambioController.cs
@@ -1,13 +1,88 @@
-﻿using System;
+﻿using Ent_Semana3.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Xml;
 
 namespace Ent_Semana3.Controllers
 {
     public class TipoCambioController : ApiController
     {
+        public RequestParam initParamsCompra()
+        {
+            return new RequestParam(
+                    "317", "05/09/2021", "05/02/2022", "Diego Morales", "N",
+                    "diego.morales22102001@gmail.com", "MG0MD0MGLO");
+        }
+
+        [HttpGet]
+        [Route("api/TipoCambio/getTipoCambioCompra")]
+        public apiResponse getTipoCambioCompra()
+        {
+            try
+            {
+                RequestParam requestParam = initParamsCompra();
+                cr.fi.bccr.gee.wsindicadoreseconomicos WSBancoCentral = new cr.fi.bccr.gee.wsindicadoreseconomicos();
+                String WSResponse = WSBancoCentral.ObtenerIndicadoresEconomicosXML(
+                    requestParam.Indicador,
+                    requestParam.FechaInicio,
+                    requestParam.FechaFinal,
+                    requestParam.Nombre,
+                    requestParam.SubNiveles,
+                    requestParam.CorreoElectronico,
+                    requestParam.Token);
+                apiResponse response = new apiResponse();
+                response.codigoRespuesta = requestParam.Indicador;
+                response.xmlResponse = WSResponse;
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.LoadXml(WSResponse);
+                return response;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+
+        public RequestParam initParamsVenta()
+        {
+            return new RequestParam(
+                    "318", "05/09/2021", "05/02/2022", "Diego Morales", "N",
+                    "diego.morales22102001@gmail.com", "MG0MD0MGLO");
+        }
+
+        [HttpGet]
+        [Route("api/TipoCambio/getTipoCambioVenta")]
+        public apiResponse getTipoCambioVenta()
+        {
+            try
+            {
+                RequestParam requestParam = initParamsVenta();
+                cr.fi.bccr.gee.wsindicadoreseconomicos WSBancoCentral = new cr.fi.bccr.gee.wsindicadoreseconomicos();
+                String WSResponse = WSBancoCentral.ObtenerIndicadoresEconomicosXML(
+                    requestParam.Indicador,
+                    requestParam.FechaInicio,
+                    requestParam.FechaFinal,
+                    requestParam.Nombre,
+                    requestParam.SubNiveles,
+                    requestParam.CorreoElectronico,
+                    requestParam.Token);
+                apiResponse response = new apiResponse();
+                response.codigoRespuesta = requestParam.Indicador;
+                response.xmlResponse = WSResponse;
+                return response;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
     }
 }
--- Ent_Semana3/Ent_Semana3/Entities/TipoCambioObj.cs
+++ Ent_Semana3/Ent_Semana3/Entities/TipoCambioObj.cs
@@ -7,5 +7,34 @@ namespace Ent_Semana3.Entities
 {
     public class TipoCambioObj
     {
+        public DateTime DES_FECHA { get; set;}
+        public int NUM_VALOR { get; set;}
+    }
+    public class RequestParam
+    {
+        public string Indicador { get; set; }
+        public string FechaInicio { get; set; }
+        public string FechaFinal { get; set; }
+        public string Nombre { get; set; }
+        public string SubNiveles { get; set; }
+        public string CorreoElectronico { get; set; }
+        public string Token { get; set; }
+
+        public RequestParam(string Indicador, string FechaInicio, string FechaFinal, string Nombre, string SubNiveles, string CorreoElectronico, string Token)
+        {
+            this.Indicador = Indicador;
+            this.FechaInicio = FechaInicio; 
+            this.FechaFinal = FechaFinal;
+            this. Nombre = Nombre;
+            this.SubNiveles = SubNiveles;
+            this.CorreoElectronico = CorreoElectronico;
+            this.Token = Token;
+        }
+    }
+
+    public class apiResponse
+    {
+        public string codigoRespuesta { get; set; }
+        public string xmlResponse { get; set; }
     }
 }
